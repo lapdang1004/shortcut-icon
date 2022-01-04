9e7c07afa1ef8cf2d06b8328e48da5aef22fe3ff
lapdang1996 <lapdang1996@gmail.com>
2022-01-04 17:11:05 +0700

    Initial commit

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
-      '/shortcut-icon/',
-      '/shortcut-icon/index.html',
-      '/shortcut-icon/index.js',
-      '/shortcut-icon/style.css',
-      '/shortcut-icon/images/fox1.jpg',
-      '/shortcut-icon/images/fox2.jpg',
-      '/shortcut-icon/images/fox3.jpg',
-      '/shortcut-icon/images/fox4.jpg',
-    ])),
-  );
-});
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(
+//     caches.open('fox-store').then((cache) => cache.addAll([
+//       '/shortcut-icon/',
+//       '/shortcut-icon/index.html',
+//       '/shortcut-icon/index.js',
+//       '/shortcut-icon/style.css',
+//       '/shortcut-icon/images/fox1.jpg',
+//       '/shortcut-icon/images/fox2.jpg',
+//       '/shortcut-icon/images/fox3.jpg',
+//       '/shortcut-icon/images/fox4.jpg',
+//     ])),
+//   );
+// });
 
 self.addEventListener('fetch', (e) => {
   console.log(e.request.url);
