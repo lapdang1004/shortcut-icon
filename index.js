591ed2882aeb7430a92369e645ff5b2539187210
lapdang1996 <lapdang1996@gmail.com>
2022-01-04 15:55:25 +0700

    update sw.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('E:\PapaGR\test\popup-create\pwa-examples\a2hs\sw.js')
+    .register('/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
 
